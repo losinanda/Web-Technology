2bbbf86c4c0bb422943e199e91bd57a404ddd373
losinanda <kadekprawira29@gmail.com>
2022-11-09 13:08:12 +0800

    Upload Midterm

---
+++ Midterm/resources/views/frontpage/cart.blade.php
@@ -0,0 +1,141 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+<div class="max-w-auto mx-auto sm:px-6 lg:px-8">
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white border-b border-gray-200">
+            <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="flex items-baseline justify-between border-b border-gray200 pt-2 pb-6">
+                    <h1 class="text-4xl font-bold tracking-tight text-gray900">Cart</h1>
+                </div>
+                <section>
+                    <div class="flex justify-around md:justify-around py-5">
+                        <div class="grid md:grid-cols-3 grid-cols-1 gap-6">
+                            <div class="font-sans pt-8 border-slate-200">
+                                <div class="max-w-sm">
+                                    <div class="md:shrink-0">
+                                        <img src="https://palm-living.com/wp-content/uploads/2018/07/Tari-Kecak-1.jpg"
+                                            alt="" class="md:h-80 md:w-56 inset-0 object-cover rounded-lg"
+                                            loading="lazy" />
+                                    </div>
+                                    <form class="pt-6">
+                                        <div class="flex flex-wrap">
+                                            <h1 class="flex font-medium text-slate-900">
+                                                Kecak Fire Gianyar
+                                            </h1>
+                                            <div class="flex space-x-4 mt-1 text-sm font-medium">
+                                                <button
+                                                    class="flex-none flex items-center justify-center w-9 h9 ml-2 rounded-full text-violet-600 bg-violet-50"
+                                                    type="button" aria-label="Like">
+                                                    <svg width="20" height="20" fill="currentColor" ariahidden="true">
+                                                        <path fill-rule="evenodd" cliprule="evenodd"
+                                                            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <p class="text-sm text-slate-500">
+                                            Adult
+                                        </p>
+                                        <div class="w-full flex-none order-1 text-3xl fontbold text-violet-600">
+                                            Rp. 120.000
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="font-sans pt-8 border-slate-200">
+                                <div class="max-w-sm">
+                                    <div class="md:shrink-0">
+                                        <img src="https://palm-living.com/wp-content/uploads/2018/07/Tari-Kecak-1.jpg"
+                                            alt="" class="md:h-80 md:w-56 inset-0 object-cover rounded-lg"
+                                            loading="lazy" />
+                                    </div>
+                                    <form class="pt-6">
+                                        <div class="flex flex-wrap">
+                                            <h1 class="flex font-medium text-slate-900">
+                                                Kecak Fire Gianyar
+                                            </h1>
+                                            <div class="flex space-x-4 mt-1 text-sm font-medium">
+                                                <button
+                                                    class="flex-none flex items-center justify-center w-9 h9 ml-2  rounded-full text-violet-600 bg-violet-50"
+                                                    type="button" aria-label="Like">
+                                                    <svg width="20" height="20" fill="currentColor" ariahidden="true">
+                                                        <path fill-rule="evenodd" cliprule="evenodd"
+                                                            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <p class="text-sm text-slate-500">
+                                            Family
+                                        </p>
+                                        <div class="w-full flex-none order-1 text-3xl fontbold text-violet-600">
+                                            Rp. 150.000
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="font-sans pt-8 border-slate-200">
+                                <div class="max-w-sm">
+                                    <div class="md:shrink-0">
+                                        <img src="https://palm-living.com/wp-content/uploads/2018/07/Tari-Kecak-1.jpg"
+                                            alt="" class="md:h-80 md:w-56 inset-0 object-cover rounded-lg"
+                                            loading="lazy" />
+                                    </div>
+                                    <form class="pt-6">
+                                        <div class="flex flex-wrap">
+                                            <h1 class="flex font-medium text-slate-900">
+                                                Kecak Fire Gianyar
+                                            </h1>
+                                            <div class="flex space-x-4 mt-1 text-sm font-medium">
+                                                <button
+                                                    class="flex-none flex items-center justify-center w-9 h9 ml-2  rounded-full text-violet-600 bg-violet-50"
+                                                    type="button" aria-label="Like">
+                                                    <svg width="20" height="20" fill="currentColor" ariahidden="true">
+                                                        <path fill-rule="evenodd" cliprule="evenodd"
+                                                            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <p class="text-sm text-slate-500">
+                                            Children
+                                        </p>
+                                        <div class="w-full flex-none order-1 text-3xl fontbold text-violet-600">
+                                            Rp. 60.000
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-fit">
+                            <h1 class=" font-semibold text-2xl">Order Summary</h1>
+                            <div class="flex justify-between mt-5 mb-5">
+                                <span class="font-semibold text-sm uppercase">Kecak Fire Gianyar Adult</span>
+                                <span class="font-semibold text-sm">Rp 120.000</span>
+                            </div>
+                            <div class="flex justify-between mb-5">
+                                <span class="font-semibold text-sm uppercase">Kecak Fire Gianyar Family</span>
+                                <span class="font-semibold text-sm">Rp 150.000</span>
+                            </div>
+                            <div class="pt-10">
+                                <label for="promo" class="font-semibold inline-block mb-3 text-sm uppercase">Promo
+                                    Code</label>
+                                <input type="text" id="promo" placeholder="Enter your code" class="p-2 text-sm w-full">
+                            </div>
+                            <button
+                                class="bg-red-500 hover:bg-red-600 px-5 py-2 mt-2 text-sm text-white uppercase">Apply</button>
+                            <div class="border-t mt-4">
+                                <div class="flex font-semibold justify-between py-6 text-sm uppercase">
+                                    <span>Total cost</span>
+                                    <span>Rp 270.000</span>
+                                </div>
+                                <button
+                                    class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Checkout</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
+    </div>
+</div>
+</div>
---
+++ Midterm/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+use Illuminate\Support\Facades\Route;
+//memanggil file controller
+use App\Http\Controllers\HomeController;
+//route sebelumnya
+Route::get('/', function () {
+return view('frontpage.landingpage');
+});
+//route yang terbaru
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/dashboard', function () {
+ return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+Route::get('/cart', function () {
+    return view('frontpage.cart');
+   });
+require __DIR__.'/auth.php';
